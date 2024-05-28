96b8d63d1d945184752f7afe3d1c4469bdc88583
duydoantechx <duy.doan@techxcorp.com>
2024-05-28 04:14:56 +0000

    Update models API and requirements files

---
+++ model.py
@@ -0,0 +1,40 @@
+import torch
+from transformers import pipeline
+
+def model_query(query:str):
+    pipe = pipeline(
+        "text-generation",
+        model = "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+        torch_dtype= torch.bfloat16,
+        device_map = "cpu"
+    )
+    
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a serious chatbot who always answers questions with highest precision and correctness. You only reply with the actual answer without repeating my question."
+        },
+        {
+            "role": "user",
+            "content": f"{query}"
+        }
+    ]
+    
+    prompt = pipe.tokenizer.apply_chat_template(
+        messages, tokenize = False, add_generation_prompt= True
+    )
+    
+    output = pipe(
+        prompt,
+        max_new_tokens = 256,
+        do_sample = True,
+        temperature = 0.7,
+        top_k = 50,
+        top_p = 0.95
+    )
+    
+    output = outputs[0]["generated_text"]
+    return output
+
+if __name__ == "__main__":
+    print(model_query("What is the meaning of life?"))
